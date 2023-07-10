ca7c318015f1e4631203424437a175c7887c270b
sainRaghuveer <raghuveersain987@gmail.com>
2023-07-10 12:46:48 +0530

    Started task app with interface and home page

---
+++ task-managment-app/src/app/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+
+interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+}
+
+const HomePage: React.FC = () => {
+  
+
+  return (
+    <div className="container">
+      
+    </div>
+  );
+};
+
+export default HomePage;
